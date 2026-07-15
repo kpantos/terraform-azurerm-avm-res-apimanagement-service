48c39a47424bd0b11b7917d91dfd5b96298d3495
Konstantinos Pantos <kostas@pantos.name>
2026-07-15 17:17:26 +0300

    Modify description for 'location' variable
    Updated the description of the 'location' variable to include 'Test'.

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 
 variable "location" {
   type        = string
-  description = "Azure region where the resource should be deployed."
+  description = "Azure region where the resource should be deployed. Test"
   nullable    = false
 }
 
